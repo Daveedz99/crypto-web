7a679d7a5235ad3c20f4d2f345a51e2747ddf6ee
davidz <davide.crespina@yahoo.it>
2021-06-02 13:19:19 +0200

    aggiunti Roadmap & Slogan components

---
+++ src/components/Roadmap.vue
@@ -0,0 +1,114 @@
+<template>
+  <h5>Customized</h5>
+  <Timeline :value="events1" align="alternate" class="customized-timeline">
+    <template #marker="slotProps">
+      <span
+        class="custom-marker p-shadow-2"
+        :style="{ backgroundColor: slotProps.item.color }"
+      >
+        <i :class="slotProps.item.icon"></i>
+      </span>
+    </template>
+    <template #content="slotProps">
+      <div class="card">
+        {{ slotProps.item.status }}
+        {{ slotProps.item.date }}
+        <img
+          v-if="slotProps.item.image"
+          :src="'demo/images/product/' + slotProps.item.image"
+          :alt="slotProps.item.name"
+          width="200"
+          class="p-shadow-2"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+        <Button label="Read more" class="p-button-text"></Button>
+      </div>
+    </template>
+  </Timeline>
+</template>
+
+<script>
+export default {
+  name: "Roadmap",
+  data() {
+    return {
+      events1: [
+        {
+          status: "Ordered",
+          date: "15/10/2020 10:30",
+          icon: "fas fa-shopping-cart",
+          color: "#9C27B0",
+          image: "game-controller.jpg",
+        },
+        {
+          status: "Processing",
+          date: "15/10/2020 14:00",
+          icon: "fas fa-cog",
+          color: "#673AB7",
+        },
+        {
+          status: "Shipped",
+          date: "15/10/2020 16:15",
+          icon: "fas fa-shopping-cart",
+          color: "#FF9800",
+        },
+        {
+          status: "Delivered",
+          date: "16/10/2020 10:00",
+          icon: "fas fa-check",
+          color: "#607D8B",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-marker {
+  display: flex;
+  width: 2rem;
+  height: 2rem;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  border-radius: 50%;
+  z-index: 1;
+}
+
+::v-deep(.p-timeline-event-content) ::v-deep(.p-timeline-event-opposite) {
+  line-height: 1;
+}
+.card {
+  background: #ffffff;
+  color: #495057;
+  box-shadow: 0 2px 1px -1px rgb(0 0 0 / 20%), 0 1px 1px 0 rgb(0 0 0 / 14%),
+    0 1px 3px 0 rgb(0 0 0 / 12%);
+  border-radius: 3px;
+}
+@media screen and (max-width: 960px) {
+  ::v-deep(.customized-timeline) {
+    .p-timeline-event:nth-child(even) {
+      flex-direction: row !important;
+
+      .p-timeline-event-content {
+        text-align: left !important;
+      }
+    }
+
+    .p-timeline-event-opposite {
+      flex: 0;
+    }
+
+    .p-card {
+      width: 0%;
+      margin-top: 1rem;
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="container">
     <span class="slogan">
-      <h1>
-      Buy and sell your music using crypto!
-      </h1>
+      <Slogan />
     </span>
-      <div class="token-address">
-       <h3>Token address: </h3>
-      <span class="token">{{token.address}}</span>
+    <div class="token-address">
+      <h3>Token address:</h3>
+      <span class="token">{{ token.address }}</span>
     </div>
     <div class="counters">
-    <Counters />
+      <Counters />
     </div>
     <div class="poocoin">
       <Button icon="fas fa-poo" class="p-button-help" label="Poocoin.com" />
-    <!-- <iframe
+      <!-- <iframe
       src="https://poocoin.app/tokens/0x1e5f009d4f8ca44b5fcc4963dd301da82b4eed09"
       frameborder="0"
       class="myIframe"
@@ -24,29 +22,32 @@
       <h3>OUR PROJECT</h3>
     </div>
     <div class="roadmap">
-    <h1>Roadmap</h1>
+      <Roadmap />
     </div>
     <div class="developers">
       DEVS--SPACE
     </div>
-    
   </div>
 </template>
 
 <script>
-import Counters from '../components/Counters.vue';
+import Counters from "../components/Counters.vue";
+import Roadmap from "../components/Roadmap.vue";
+import Slogan from "../components/Slogan.vue";
 export default {
   name: "Home",
   data() {
     return {
-     token:{
-       address: '0x83testdf1a7c56e7test7d321312TEST52e0a2aad0e'
-     }
+      token: {
+        address: "0x83testdf1a7c56e7test7d321312TEST52e0a2aad0e",
+      },
     };
   },
-  components:{
-    Counters
-  }
+  components: {
+    Counters,
+    Roadmap,
+    Slogan,
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -54,19 +55,19 @@ export default {
   height: 60vh;
   width: 60vw;
 }
-.token-address{
-  margin: 1rem 1rem 1rem 1rem
+.token-address {
+  margin: 1rem 1rem 1rem 1rem;
 }
-.token{
+.token {
   color: black;
   padding: 0.4rem;
   border-radius: 10px;
   background-color: rgba(51, 48, 48, 0.582);
 }
-.counters{
+.counters {
   margin: 4rem 1rem 4rem 1rem;
 }
-.poocoin{
+.poocoin {
   margin: 1rem 1rem 1rem 1rem;
 }
 .container {
