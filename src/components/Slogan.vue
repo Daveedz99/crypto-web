f2e3a718aeb2036b35777d53104762427124b0f3
davidz <davide.crespina@yahoo.it>
2021-06-02 14:03:30 +0200

    save

--- src/components/Slogan.vue
+++ src/components/Slogan.vue
@@ -2,10 +2,10 @@
   <h1>
     Buy and sell your music using crypto!
   </h1>
-  <Button icon="fas fa-cash" class="p-button-lg" label="Buy DrillWallet" />
+  <Button icon="fas fa-wallet" class="p-button-lg p-button-help" label="Buy DrillWallet" />
   <Button
     icon="fab fa-telegram"
-    class="p-button-outlined p-button-lg p-ml-3"
+    class="p-button-outlined p-button-lg p-ml-3 p-button-help"
     label="Join Telegram"
   />
 </template>
